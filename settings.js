09252ce89d811104239efaa8f09c3f467f07b968
Ryan Always JN <112480275+YannPrtm@users.noreply.github.com>
2022-12-11 20:01:46 +0800

    Add files via upload

--- settings.js
+++ settings.js
@@ -26,24 +26,24 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/@ChZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Ryan-Bot" //namabot kalian
+global.ownername= "Ryan DCoder ID" //nama kalian
+global.myweb ="https://github.com/YannPrtm" //bebas asal jan hapus
+global.youtube = "https://youtu.be/BHaXm9DXv0w" //bebas asal jan hapus
+global.myweb2 = "https://youtu.be/BHaXm9DXv0w" //bebas
+global.email = "ryandeveloper@xyz.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326","622150996855"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.premium = ['6282252285143'] //premium user
+global.owner = ["6282252285143","6282252285143"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "6282252285143" // nomor wa kalian
+global.ownernomerr = "+6282252285143" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.packname = '© Ryan-BOTZོ' //sticker wm ubah
+global.author = 'Di Buat Oleh RyanDCoder\nWA : 082252285143' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 global.multiplier = 69
